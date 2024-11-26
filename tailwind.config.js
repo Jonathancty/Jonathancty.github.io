fec37cd768932a22d0fb8b6f74e930367f8836f5
Jonathancty <jc@enochan.net>
2024-11-26 22:06:49 +0000

    made the initial structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,24 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-shadow": {
+          textShadow: "0 2px 4px rgba(0, 0, 0, 0.10)",
+        },
+        ".text-shadow-md": {
+          textShadow: "0 4px 6px rgba(0, 0, 0, 0.10)",
+        },
+        ".text-shadow-lg": {
+          textShadow: "0 10px 15px rgba(0, 0, 0, 0.10)",
+        },
+        ".text-shadow-xl": {
+          textShadow: "0 20px 25px rgba(0, 0, 0, 0.10)",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };

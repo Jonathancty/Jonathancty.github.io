6f171eddb39909deeaf2e57979d8e3102bf4512a
Jonathancty <jc@enochan.net>
2024-11-27 20:56:45 +0000

    updated skills and education part

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
         rubik: ["Rubik", "sans-serif"],
       },
+      animation: {
+        "gradient-x": "gradient-x 15s ease infinite",
+      },
+      keyframes: {
+        "gradient-x": {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+        },
+      },
     },
   },
   plugins: [
